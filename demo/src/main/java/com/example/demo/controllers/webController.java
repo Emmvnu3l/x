d582f9b1ba23093fb150793854ba867f8fe33678
Emmvnu3l <emm.moreno@duocuc.cl>
2025-06-22 12:58:05 -0400

    mod web

--- demo/src/main/java/com/example/demo/controllers/webController.java
+++ demo/src/main/java/com/example/demo/controllers/webController.java
@@ -1,15 +1,9 @@
 package com.example.demo.controllers;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.example.demo.entities.Usuario;
 
 @Controller
 public class webController {
